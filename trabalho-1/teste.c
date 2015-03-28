6a904020bbe0e11b9eb66c21fee12bcd11022c54
CainaTrevisan <cainact@gmail.com>
2015-03-27 23:52:08 -0300

    Input e Output são arquivos ao invés da entrada e saída padrão para testar. Input é cidades.dot. Output é c.out

--- trabalho-1/teste.c
+++ trabalho-1/teste.c
@@ -4,5 +4,16 @@
 //------------------------------------------------------------------------------
 int main(void) {
 
-  return ! destroi_grafo(escreve_grafo(stdout, le_grafo(stdin)));
+  FILE *file_out = fopen("c.out","w+");
+
+  FILE *file_in = fopen("cidades.dot","r");
+  if (!file_in)
+      return -1;
+
+  int r = destroi_grafo(escreve_grafo(file_out, le_grafo(file_in)));
+
+  fclose(file_out);
+  fclose(file_in);
+
+  return r;
 }
